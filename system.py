ea9a05a0bccb58989b04d9c0cb869a51d6117067
Richard Gieg <richgieg@users.noreply.github.com>
2015-08-26 23:43:38 -0700

    Add docstrings to system module

--- system.py
+++ system.py
@@ -4,13 +4,16 @@ import video
 
 
 class System():
+    """Represents a computer system."""
 
     def __init__(self):
+        """Creates and initializes the system components."""
         self._cpu = cpu.Cpu()
         self._memory_controller = memory.MemoryController()
         self._video_controller = video.VideoController()
 
     def power_on(self):
+        """Powers on the system components."""
         print("System.power_on()")
         self._cpu.power_on()
         self._memory_controller.power_on()
