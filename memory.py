e91d2ce03a3ca97f1989de5c2efd1450e5aaab28
Richard Gieg <richgieg@users.noreply.github.com>
2015-08-26 23:36:49 -0700

    Add docstrings to memory module

--- memory.py
+++ memory.py
@@ -1,4 +1,6 @@
 class MemoryController():
+    """Represents a computer system's memory controller."""
 
     def power_on(self):
+        """Powers on this memory controller."""
         print("MemoryController.power_on()")
