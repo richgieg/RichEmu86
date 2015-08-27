63dd1ebca2002f24531f2725938be2dbc1018afc
Richard Gieg <richgieg@users.noreply.github.com>
2015-08-26 22:53:39 -0700

    Add memory module

--- system.py
+++ system.py
@@ -1,10 +1,13 @@
+import memory
 import video
 
 
 class System():
 
     def __init__(self):
+        self.__memory_controller = memory.MemoryController()
         self.__video_controller = video.VideoController()
 
     def power_on(self):
+        self.__memory_controller.power_on()
         self.__video_controller.power_on()
